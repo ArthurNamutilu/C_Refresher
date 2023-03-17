1406838e128a6c6f4526a5b71b2b95f6777d4ae2
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-03-17 12:09:19 +0300

    added hello file

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * printf - outputs to the screen
+  * return: Always 0 (success)
+  */
+
+int main(void)
+{
+	printf("Hello world\n");
+	return (0);
+}
