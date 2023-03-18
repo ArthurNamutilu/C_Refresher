b571047cac42fdcfd655c012e4d62a7cdee90a48
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-03-19 00:42:07 +0300

    find maximum number using if

--- if_maxNum.c
+++ if_maxNum.c
@@ -13,11 +13,11 @@ int max(int num1, int num2, int num3)
 {
 	int result;
 
-	if (num1 > num2 && num1 > num3)
+	if (num1 >= num2 && num1 >= num3)
 	{
 		result = num1;
 	}
-	else if (num2 > num1 && num2 > num3)
+	else if (num2 >= num1 && num2 >= num3)
 	{
 		result = num2;
 	}
