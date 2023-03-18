f296b501df66c0418f07ed5feea8eb3987353bc5
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-03-18 10:24:36 +0300

    modified output files

---
+++ alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - Begining of code
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int n;
+
+	for (n = 65; n <= 90; n++)
+	{
+		putchar(n);
+	}
+	return (0);
+}
