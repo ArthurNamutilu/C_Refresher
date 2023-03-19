5afe55e1fa81b74bfdaaba7e07d73339bba53b9e
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-03-20 00:20:38 +0300

    pointers

---
+++ pointers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - start
+  *
+  * Return: 0 success
+  */
+
+int main(void)
+{
+	int num = 14;
+	int * pNum = &num;
+	double gpa = 8.7;
+	double * pGpa = &gpa;
+	char grade = 'A';
+	char * pGrade = &grade;
+
+	printf("num's memory adress: %p\n", &num);
+
+	return (0);
+}
