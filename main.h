d8fcfec253ca800639351dfb13c374e56b856763
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-04-03 07:15:33 +0300

    updated main header file

--- main.h
+++ main.h
@@ -5,5 +5,6 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
-
+char *_strncat(char *dest, char *src, int n);
+void print_buffer(char *b, int size)
 #endif /* MAIN_H */
